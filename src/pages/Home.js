6db27fd81fab0f12ad11c0858e46e889f6bf8a53
keemHwa <23390@naver.com>
2023-10-13 23:59:55 +0900

    :sparkles: Home page Add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
+import { Fragment, useEffect } from "react";
+import { mainDiary } from "../data/home";
+import DiaryMaker from "../components/DiaryMaker";
+import { useDispatch } from "react-redux";
+import { uiActions } from "../store/ui-slice";
+
 const Home = () => {
-  return <></>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      uiActions.checkWritableMenu({
+        status: false,
+      })
+    );
+  }, [dispatch]);
+  return (
+    <Fragment>
+      <DiaryMaker key={mainDiary._id} {...mainDiary} />
+    </Fragment>
+  );
 };
 
 export default Home;
